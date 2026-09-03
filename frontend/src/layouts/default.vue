8ce742fb4d3e6ad6a318288a08aef9171973f64e
Clickadelic <toby.hopp@gmail.com>
2026-09-03 14:53:19 +0200

    AppHeader imported

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-
+import AppHeader from "@/components/AppHeader.vue";
 import AppFooter from "@/components/AppFooter.vue";
 </script>
 
 <template>
 	<div class="default-layout">
+		<AppHeader />
 		<main class="container mx-auto">
 			<router-view />
 		</main>
